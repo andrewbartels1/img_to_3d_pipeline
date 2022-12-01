b32bc47aceeb2f7c0359301d586feefb4dc3d08e
andrew.bartels <andrew.bartels@geomdata.com>
2022-12-01 14:31:43 -0800

    pushing update

---
+++ src/im23D_pipeline/pydantic_models/model_descriptions.py
@@ -0,0 +1,15 @@
+"""
+@Author: andrew.bartels  (andrew.bartels@gatech.edu) 
+@Date: 2022-12-01 13:39:08  
+@Last Modified by:   andrew.bartels  
+@Last Modified time: 2022-12-01 13:39:08 
+@license: see LICENSE for more details 
+"""
+
+# All model descriptions for the pydantic models should go here
+field_description = "test model desscription"
+dataset_folder_description = "pathlib.Path object that holds the folder where the dataset lives, for example `../data/ShapeNetCore.v2/` will be used for the `"
+mesh_type_description = "Enumerator object that will only accept the mesh type that will be preprocessed by the dataset class and loaded into the dataloader"
+
+
+
--- src/im23D_pipeline/pydantic_models/models.py
+++ src/im23D_pipeline/pydantic_models/models.py
@@ -1,1 +1,35 @@
-from pydantic import BaseModel, FilePath, validator, AnyUrl, Field
+from pydantic import BaseModel, FilePath, DirectoryPath, validator, AnyUrl, Field, root_validator
+from im23D_pipeline.pydantic_models.model_descriptions import *
+from pathlib import Path
+from enum import Enum
+
+
+class MeshTypeChoices(Enum):
+    mesh = "mesh"
+    point_cloud = "point_cloud"
+    sdf = "sdf"
+    voxel = "voxel"
+    
+class BaseDatasetModel(BaseModel):
+    """Base model for dataset inputs"""
+    dataset_folder: DirectoryPath = Field(DirectoryPath, description=dataset_folder_description, cli=("-df", "--dataset-folder"))
+    mesh_type: MeshTypeChoices = Field(MeshTypeChoices.mesh, description=mesh_type_description, cli=("", ""))
+    
+    @root_validator()
+    def validate_fields(cls, values):
+        """This validator can check values based on other values"""
+        return values
+        
+
+class BaseAtomicDataObjectModel(BaseModel):
+    """This will be the smalles `set` of items will need, think what gets fed into the model, what comes out, and anything it needs to get compared to
+    This could be anything from an object file, an image (or set of images), camera angle, (maybe even textures) for scene generation via `trimesh`.
+    ['synset_id', 'model_id', 'verts', 'faces', 'label', 'images', 'R', 'T', 'K', 'voxel_coords', 'voxels', 'mesh']
+    is an example from the R2N2 Custom Dataset(and there's too much here) for one thing this should have to pass through a model and get a loss!"""        
+    pass
+
+class TrainerPydanticModel(BaseModel):
+    """all the hyperparameter inputs should output some sort of file correlating to the run with the inputs --> output/performance + model
+    i.e. this will be the cli with things like epochs, d"""
+    pass
+    
