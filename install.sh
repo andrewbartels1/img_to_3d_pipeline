592d7fc2abbfec18f0c1e3f6b6abe397e23f617d
andrew.bartels <andrew.bartels@geomdata.com>
2022-11-30 16:45:46 -0800

    updating, finally starting the dev work now

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 conda env create -f im23D_pipeline.yml
 source activate im23D_pipeline
+echo $CONDA_PREFIX
 pip install -e .[dev,testing]
 python -m ipykernel install --user --name im23D_pipeline --display-name "Python (im23D_pipeline)"
